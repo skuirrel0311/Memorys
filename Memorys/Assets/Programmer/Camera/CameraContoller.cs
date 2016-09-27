7c1af64d8291fb9c2481199cc49593498703ce9e
skuirrel0311 <rureikus7223@gmail.com>
2016-09-27 12:27:01 +0900

    【Camera】カメラの基本的な操作を加えました。
    キーボードでは動かないぞ。

--- Memorys/Assets/Programmer/Camera/CameraContoller.cs
+++ Memorys/Assets/Programmer/Camera/CameraContoller.cs
@@ -5,23 +5,41 @@ public class CameraContoller : MonoBehaviour
 {
     [SerializeField]
     GameObject player = null;
+    [SerializeField]
+    float distance = 7;    //カメラとプレイヤーの距離
+    [SerializeField]
+    float height = 3;
+    [SerializeField]
+    float rotationSpeed = 250;
+    
     PlayerController controller;
-    Vector3 oldPosition;
+    float rotation;
 
     void Start()
     {
-        oldPosition = Vector3.zero;
+        rotation = 270;
         controller = player.GetComponent<PlayerController>();
+        transform.position = GetCameraPositoin(rotation);
     }
     
     void Update()
     {
-        Vector3 movement = player.transform.position - oldPosition;
-        //movement.y = 0;
+        float leftStickX = Input.GetAxis("Horizontal2") * -1;
+        //Debug.Log("leftStickX = " + leftStickX);
         
-        //プレイヤーが移動した分だけカメラも移動する
-        transform.position += movement;
-        
-        oldPosition = player.transform.position;
+        rotation += leftStickX * rotationSpeed * Time.deltaTime;
+        transform.position = GetCameraPositoin(rotation);
+        transform.LookAt(player.transform.position + Vector3.up);
+    }
+
+    //指定された角度のときのカメラの位置を返す
+    Vector3 GetCameraPositoin(float rotation)
+    {
+        Vector3 cameraPosition = player.transform.position;
+        float temp = rotation * Mathf.Deg2Rad;
+        cameraPosition.x += Mathf.Cos(temp) * distance;
+        cameraPosition.z += Mathf.Sin(temp) * distance;
+        cameraPosition.y += height;
+        return cameraPosition;
     }
 }
--- Memorys/Assets/Programmer/Player/PlayerController.cs
+++ Memorys/Assets/Programmer/Player/PlayerController.cs
@@ -69,6 +69,10 @@ public class PlayerController : MonoBehaviour
                 {
                     currentState = PlayerState.Land;
                 }
+                else
+                {
+                    Debug.Log("No Ground Hit");
+                }
                 break;
         }
 
