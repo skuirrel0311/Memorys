3f41a371d4204c03b3fce68a32e9591dfcebf8bb
TachibanaKoki <standchat0820@gmail.com>
2016-10-20 22:02:38 +0900

    【Select】セレクト画面をやってたど未完

--- Memorys/Assets/Programmer/SceneManager/SelectManager.cs
+++ Memorys/Assets/Programmer/SceneManager/SelectManager.cs
@@ -9,11 +9,15 @@ public class SelectManager : MonoBehaviour
     [SerializeField]
     Animator m_BookAnim;
 
-    int m_SelectNumber = 1;
+    private FusenSpawner m_FusenSpawner;
+    public int m_SelectNumber = 1;
+    bool isInputAxis;
     // Use this for initialization
     void Start ()
     {
         m_SelectNumber = 1;
+        isInputAxis = false;
+        m_FusenSpawner = GetComponent<FusenSpawner>();
 	}
 	
 	// Update is called once per frame
@@ -28,17 +32,25 @@ public class SelectManager : MonoBehaviour
         float v = MyInputManager.GetAxis(MyInputManager.Axis.LeftStick).x;
         if (v == 0)
         {
+            isInputAxis = false;
             return;
         }
+        if (isInputAxis) return;
+        isInputAxis = true;
+
         if (v > 0)
         {
+
             m_SelectNumber = (int)Mathf.Max(1, (float)m_SelectNumber - 1);
-            m_BookAnim.CrossFade("L_R",0.1f,0);
+            m_FusenSpawner.SetAnimationRoot(m_SelectNumber - 1);
+            m_BookAnim.Play("L_R",0);
         }
         else
         {
             m_SelectNumber = (int)Mathf.Min((float)MaxStage, (float)m_SelectNumber + 1);
-            m_BookAnim.CrossFade("R_L", 0.1f, 0);
+            m_FusenSpawner.SetAnimationRoot(m_SelectNumber-1);
+            m_BookAnim.Play("R_L", 0);
+
         }
     }
 
--- Memorys/Assets/Programmer/StageSelect/Scripts/FusenSpawner.cs
+++ Memorys/Assets/Programmer/StageSelect/Scripts/FusenSpawner.cs
@@ -1,31 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class FusenSpawner : MonoBehaviour
 {
     [SerializeField]
-    GameObject fusen;
+    GameObject Fusen;
     [SerializeField]
-    Vector3 LeftAnchor = new Vector3(0,0,0);
+    GameObject FusenRoot;
     [SerializeField]
-    Vector3 RightAnchor = new Vector3(0,0,0);
+    public Vector3 LeftAnchor = new Vector3(0,0,0);
+    [SerializeField]
+    public Vector3 RightAnchor = new Vector3(0,0,0);
     [SerializeField]
     int maxStage=1;
 
-    List<GameObject> fusens;
-	// Use this for initialization
-	void Start ()
+    [HideInInspector]
+    public List<GameObject> fusens;
+
+    bool isSetRoot = false;
+    // Use this for initialization
+    void Start ()
     {
         fusens = new List<GameObject>();
 	    for(int i = 0;i < maxStage;i++)
         {
-            fusens.Add(GameObject.Instantiate(fusen,RightAnchor +  new Vector3(0.0f,-0.006f*i,-0.05f*i),Quaternion.identity) as GameObject);
+            fusens.Add(GameObject.Instantiate(Fusen,RightAnchor +  new Vector3(0.0f,-0.006f*i,-0.05f*i),Quaternion.identity) as GameObject);
         }
 	}
+
+    void PositionSet()
+    {
+        for (int i = 0; i < maxStage; i++)
+        {
+            if (GetComponent<SelectManager>().m_SelectNumber - 1 <= i)
+            {
+                fusens[i].transform.position = RightAnchor + new Vector3(0.0f, -0.006f * i, -0.05f * i);
+            }
+            else
+            {
+                fusens[i].transform.position = LeftAnchor+ new Vector3(0.0f, -0.01f, -0.05f * i);
+            }
+            fusens[i].transform.rotation = Quaternion.identity;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
 	
 	}
+    
+    IEnumerator ParentDetach()
+    {
+        if (isSetRoot) yield return null;
+        isSetRoot = true;
+        yield return  new  WaitForSeconds(1.0f);
+        FusenRoot.transform.DetachChildren();
+        PositionSet();
+        isSetRoot = false;
+        yield return null;
+    }
+
+    public void SetAnimationRoot(int index)
+    {
+        fusens[index-1].transform.parent = FusenRoot.transform;
+        StartCoroutine("ParentDetach");
+    }
 }
