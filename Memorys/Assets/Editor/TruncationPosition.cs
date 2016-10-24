ab3258192ea779f59b7658fb71b2fb9198c6c8b4
Naoto Ando <suniwocarina@gmail.com>
2016-10-24 21:38:18 +0900

    【レベルデザイン】ステージのプレハブの一部を更新（まだ途中）。また、GameObjectのPositionの小数を切り捨てるスクリプトを追加。

---
+++ Memorys/Assets/Editor/TruncationPosition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public static class EditorTest01
+{
+    //小数点を切り捨て
+    [MenuItem("Tools/TruncationPosition")]
+    static void TruncationPosition()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Vector3 v = obj.transform.localPosition;
+            int x = (int)v.x;
+            int y = (int)v.y;
+            int z = (int)v.z;
+            obj.transform.localPosition = new Vector3((float)x, (float)y, (float)z);
+        }
+    }
+}
