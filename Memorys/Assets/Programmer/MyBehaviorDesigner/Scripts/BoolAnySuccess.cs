c5519f943515e21d024bb1a19c01c6d01a9a2712
skuirrel <rureikus7223@gmail.com>
2016-11-09 17:17:13 +0900

    【Enemy】敵の修正２

---
+++ Memorys/Assets/Programmer/MyBehaviorDesigner/Scripts/BoolAnySuccess.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class BoolAnySuccess : Conditional
+{
+    public SharedBool[] boolValues;
+
+    public override TaskStatus OnUpdate()
+    {
+        foreach(SharedBool v in boolValues)
+        {
+            if (v.Value) return TaskStatus.Success;
+        }
+
+        return TaskStatus.Failure;
+    }
+}
