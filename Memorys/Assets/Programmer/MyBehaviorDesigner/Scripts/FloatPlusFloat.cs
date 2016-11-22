a3c4c702a35dc15b98854441500a3d7e6fddcd80
kazuki <rureikus7223@gmail.com>
2016-11-22 16:32:08 +0900

    【Enemy】AIの作成途中(未完成)

---
+++ Memorys/Assets/Programmer/MyBehaviorDesigner/Scripts/FloatPlusFloat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class FloatPlusFloat : Action
+{
+    public SharedFloat floatValue;
+    public SharedFloat result;
+    public bool IsMainus = false;
+
+    public override TaskStatus OnUpdate()
+    {
+        if (floatValue == null || result == null) return TaskStatus.Failure;
+
+        if (IsMainus) floatValue.SetValue(floatValue.Value * -1.0f);
+        float temp = floatValue.Value + result.Value;
+        result.SetValue(temp);
+        return TaskStatus.Success;
+    }
+}
---
+++ Memorys/Assets/Programmer/MyBehaviorDesigner/Scripts/IntPlusInt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class IntPlusInt : Action 
+{
+    public SharedInt intValue;
+    public SharedInt result;
+    public bool IsMainus = false;
+
+    public override TaskStatus OnUpdate()
+    {
+        if (intValue == null || result == null) return TaskStatus.Failure;
+
+        if (IsMainus) intValue.SetValue(intValue.Value * -1);
+        int temp = intValue.Value + result.Value;
+        result.SetValue(temp);
+        return TaskStatus.Success;
+    }
+}
