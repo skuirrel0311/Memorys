64f42327f4c8b7c0fd8bea41174e99e7b2448389
skuirrel0311 <rureikus7223@gmail.com>
2016-12-06 13:50:40 +0900

    【Input】InputManagerを複数コントローラーを想定した設計に改造しました。

--- Memorys/Assets/Programmer/Input/MyInputManager.cs
+++ Memorys/Assets/Programmer/Input/MyInputManager.cs
@@ -8,8 +8,8 @@ public class MyInputManager : BaseManager<MyInputManager>
     public enum Trigger { LeftTrigger, RightTrigger }
     public enum Axis { LeftStick, RightStick, Dpad }
 
-    private static GamepadState currentState;
-    private static GamepadState oldState;
+    private static GamepadState[] currentState = new GamepadState[4];
+    private static GamepadState[] oldState = new GamepadState[4];
 
     public void Awake()
     {
@@ -21,50 +21,55 @@ public class MyInputManager : BaseManager<MyInputManager>
         }
 
         DontDestroyOnLoad(this.gameObject);
-        currentState = GamePad.GetState(GamePad.Index.One);
+        for (int i = 0; i < currentState.Length; i++)
+        {
+            currentState[i] = GamePad.GetState((GamePad.Index)i);
+        }
         oldState = currentState;
     }
 
     public void Update()
     {
         oldState = currentState;
-        currentState = GamePad.GetState(GamePad.Index.One);
+        for (int i = 0; i < currentState.Length; i++)
+        {
+            currentState[i] = GamePad.GetState((GamePad.Index)i);
+        }
     }
 
-    public static bool GetButton(Button button)
+    public static bool GetButton(Button button,GamePad.Index index = GamePad.Index.One)
+    {
+        return GamePad.GetButton((GamePad.Button)button, index);
+    }
+
+    public static bool GetButtonDown(Button button,GamePad.Index index = GamePad.Index.One)
+    {
+        return GamePad.GetButtonDown((GamePad.Button)button, index);
+    }
+
+    public static bool GetButtonUp(Button button, GamePad.Index index = GamePad.Index.One)
     {
         
-        return GamePad.GetButton((GamePad.Button)button, GamePad.Index.One);
+        return GamePad.GetButtonUp((GamePad.Button)button, index);
     }
 
-    public static bool GetButtonDown(Button button)
+    public static Vector2 GetAxis(Axis axis, GamePad.Index index = GamePad.Index.One)
     {
-        return GamePad.GetButtonDown((GamePad.Button)button, GamePad.Index.One);
+        return GamePad.GetAxis((GamePad.Axis)axis, index);
     }
 
-    public static bool GetButtonUp(Button button)
+    public static float GetTrigger(Trigger trigger, GamePad.Index index = GamePad.Index.One)
     {
-        
-        return GamePad.GetButtonUp((GamePad.Button)button, GamePad.Index.One);
+        return GamePad.GetTrigger((GamePad.Trigger)trigger, index);
     }
 
-    public static Vector2 GetAxis(Axis axis)
-    {
-        return GamePad.GetAxis((GamePad.Axis)axis, GamePad.Index.One);
-    }
-
-    public static float GetTrigger(Trigger trigger)
-    {
-        return GamePad.GetTrigger((GamePad.Trigger)trigger, GamePad.Index.One);
-    }
-
-    public static bool IsStickDown(StickDirection direction)
+    public static bool IsStickDown(StickDirection direction, GamePad.Index index = GamePad.Index.One)
     {
         Vector2 stick;
         if (direction >= (StickDirection)4)
-            stick = GamePad.GetAxis(GamePad.Axis.RightStick, GamePad.Index.One);
+            stick = GamePad.GetAxis(GamePad.Axis.RightStick, index);
         else
-            stick = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One);
+            stick = GamePad.GetAxis(GamePad.Axis.LeftStick, index);
         float dead = 0.3f;
         switch(direction)
         {
@@ -84,20 +89,20 @@ public class MyInputManager : BaseManager<MyInputManager>
         return false;
     }
 
-    public static bool IsJustStickDown(StickDirection direction)
+    public static bool IsJustStickDown(StickDirection direction, GamePad.Index index = GamePad.Index.One)
     {
         Vector2 stick,oldStick;
         float dead = 0.3f;
 
-        if (direction >= (StickDirection)4)
+        if (direction >= StickDirection.RightStickRight)
         {
-            stick = currentState.rightStickAxis;
-            oldStick = oldState.rightStickAxis;
+            stick = currentState[(int)index].rightStickAxis;
+            oldStick = oldState[(int)index].rightStickAxis;
         }
         else
         {
-            stick = currentState.LeftStickAxis;
-            oldStick = oldState.LeftStickAxis;
+            stick = currentState[(int)index].LeftStickAxis;
+            oldStick = oldState[(int)index].LeftStickAxis;
         }
         
         switch (direction)
@@ -118,28 +123,28 @@ public class MyInputManager : BaseManager<MyInputManager>
         return false;
     }
 
-    public static bool IsTriggerDown(Trigger trigger)
+    public static bool IsTriggerDown(Trigger trigger, GamePad.Index index = GamePad.Index.One)
     {
-        float t = GamePad.GetTrigger((GamePad.Trigger)trigger, GamePad.Index.One);
+        float t = GamePad.GetTrigger((GamePad.Trigger)trigger, index);
         float dead = 0.05f;
 
         return t > dead;
     }
 
-    public static bool IsJustTriggerDown(Trigger trigger)
+    public static bool IsJustTriggerDown(Trigger trigger, GamePad.Index index = GamePad.Index.One)
     {
         float t,oldT;
         float dead = 0.05f;
         
         if (trigger == Trigger.LeftTrigger)
         {
-            t = currentState.LeftTrigger;
-            oldT = oldState.LeftTrigger;
+            t = currentState[(int)index].LeftTrigger;
+            oldT = oldState[(int)index].LeftTrigger;
         }
         else
         {
-            t = currentState.RightTrigger;
-            oldT = oldState.RightTrigger;
+            t = currentState[(int)index].RightTrigger;
+            oldT = oldState[(int)index].RightTrigger;
         }
         
         return t > dead && oldT <= dead;
