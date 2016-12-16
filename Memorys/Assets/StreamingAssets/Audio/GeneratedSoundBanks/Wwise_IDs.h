15bbbec6680d54c1caa1f829f200d916b107bce1
Naoto Ando <suniwocarina@gmail.com>
2016-12-16 19:30:42 +0900

    【サウンド】素材リストおよび仮素材を追加しましたん

--- Memorys/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Memorys/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,59 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BABBLE = 4051581707U;
+        static const AkUniqueID BGM_HELP = 2162141673U;
+        static const AkUniqueID BGM_MAIN1 = 652491268U;
+        static const AkUniqueID BGM_MAIN2 = 652491271U;
+        static const AkUniqueID BGM_RESULT = 4239768699U;
+        static const AkUniqueID BGM_SELECT = 2333034960U;
+        static const AkUniqueID BGM_TITLLE = 3608707068U;
+        static const AkUniqueID ENEMY_ATTACK = 1781417190U;
+        static const AkUniqueID ENEMY_MOVE = 2876602209U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GAMECLEAR = 1397260774U;
+        static const AkUniqueID GAMEOVER = 4158285989U;
+        static const AkUniqueID GAMESTART = 4058101365U;
+        static const AkUniqueID GET_SWITCH = 3960233822U;
+        static const AkUniqueID MENU_CANCEL = 3180909377U;
+        static const AkUniqueID MENU_DECISION = 191248761U;
+        static const AkUniqueID MENU_PAGE = 205371738U;
+        static const AkUniqueID MENU_SELECT = 4203375351U;
+        static const AkUniqueID OPEN_DOOR = 2934549620U;
+        static const AkUniqueID PLAYER_CLIMB = 3549554524U;
+        static const AkUniqueID PLAYER_DISCOVERY = 1063083385U;
+        static const AkUniqueID PLAYER_JUMP = 1305133589U;
+        static const AkUniqueID PLAYER_LANDING = 1282282006U;
+        static const AkUniqueID PLAYER_SEARCH = 3889015721U;
+        static const AkUniqueID TOTEM_ACTIVATION = 3042685367U;
+        static const AkUniqueID TOTEM_LASER = 2329625548U;
+        static const AkUniqueID TOTEM_LASER_CHARGE = 3607114015U;
+        static const AkUniqueID TOTEM_LASER_IMPACT = 3343999249U;
+        static const AkUniqueID TOTEM_MOVE = 2364382980U;
+        static const AkUniqueID TOTEM_SHOT = 2592508863U;
+        static const AkUniqueID TOTEM_SHOT_IMPACT = 2455001044U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BGM_CHANGE
+        {
+            static const AkUniqueID GROUP = 1211814768U;
+
+            namespace STATE
+            {
+                static const AkUniqueID EMERGENCY = 867808496U;
+                static const AkUniqueID NORMAL = 1160234136U;
+                static const AkUniqueID STEALTH = 2909291642U;
+            } // namespace STATE
+        } // namespace BGM_CHANGE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BGM_TRANSITION = 1748736713U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
